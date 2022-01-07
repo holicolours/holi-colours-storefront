5cc6fd07ce7640c9e3ef9d6c97626ec733086b7d
Magesh S <holicoloursit@gmail.com>
2022-01-07 22:47:03 +0530

    firebase-messaging-sw.js fix

--- .eleventy.js
+++ .eleventy.js
@@ -5,13 +5,13 @@ module.exports = function (config) {
     config.addPassthroughCopy("assets");
     config.addFilter("search", searchFilter);
 
-    config.on('beforeBuild', () => {
-        console.log('=================beforeBuild=================');
+    config.on('afterBuild', () => {
+        console.log('=================afterBuild=================');
         console.log('Copying firebase-messaging-sw.js...');
         fs.copyFile('assets/js/firebase-messaging-sw.js', '_site/firebase-messaging-sw.js', (err) => {
             if (err) console.log('Error copying firebase-messaging-sw.js:' + err);
             console.log('Copied firebase-messaging-sw.js!');
-            console.log('=================beforeBuild=================');
+            console.log('=================afterBuild=================');
         });
     });
 };
